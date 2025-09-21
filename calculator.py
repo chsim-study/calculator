7d8114b9ecfeb388a4dc10298428f78447256401
심채훈 <tlacogns@gmail.com>
2025-09-21 18:30:05 +0900

    add: 숫자입력기능

---
+++ calculator.py
@@ -0,0 +1,14 @@
+class Calculator:
+    def sum(a, b):
+        return a + b
+
+    def sub(a, b):
+        return a - b
+
+    def mul(a, b):
+        return a * b
+
+    def div(a, b):
+        if b != 0:
+            return a / b
+        return None
--- main.py
+++ main.py
@@ -1,21 +1,29 @@
+from calculator import Calculator
+
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
+
+save_list = []
 
 class Calculate(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("ui/cal.ui", self)
         self.setWindowTitle("Calculator")
+        
+        self.num_buttons = [
+            self.btn_num0, self.btn_num1, self.btn_num2, self.btn_num3, self.btn_num4,
+            self.btn_num5, self.btn_num6, self.btn_num7, self.btn_num8, self.btn_num9
+        ]
 
-    def calculate(self):
-        try:
-            num1 = float(self.lineEdit.text())
-            num2 = float(self.lineEdit_2.text())
-            result = num1 + num2
-            self.label_result.setText(f"Result: {result}")
-        except ValueError:
-            self.label_result.setText("Invalid input")
+        for i, btn in enumerate(self.num_buttons):
+            btn.clicked.connect(lambda _, x=i: self.add_num(str(x)))
+
+    def add_num(self, num: str):
+        current_text = self.edit_result.text()
+        self.edit_result.setText(current_text + num)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
